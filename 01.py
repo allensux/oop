ba361d9a15dfb7d6c75b3491d694a02d4867a286
sun-lm <sun-lm@neusoft.com>
2018-12-21 09:39:29 +0800

    test

--- 01.py
+++ 01.py
@@ -1,2 +1,23 @@
-def test():
-    pass
+class test():
+    age = 18
+    name = "damin"
+
+    def student(self):
+        self.age = 20
+        self.name = "sunlimin"
+
+# 此时，test为类的实例
+print(test.name)
+print(test.age)
+print(id(test.age))
+print(id(test.name))
+
+print("*" * 20)
+
+# t 代表test的一类中一个具体的对象  即对象的实例
+t =  test()
+
+print(t.name)
+print(t.age)
+print(id(t.age))
+print(id(t.name))
